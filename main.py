a680d0c1d5adaf3039deb681b0983e8d58f1e1f1
Ahmed Al-rufai <aalrefaei93@gmail.com>
2024-01-11 20:52:28 +0200

    Design the UI prompting precess

--- main.py
+++ main.py
@@ -14,26 +14,22 @@ CLARIFAI_PAT = clarifai_pat
 # Model parameters or mixtral
 inference_params = dict(temperature=0.7, max_tokens=200, top_k = 50, top_p= 0.95)
 
+
 st.title("Chatbot with Mixtral")
-
-st.metric(label="Score", value="1/4", delta="+25%")
-
-st.metric(label="Score", value="2/4", delta="-25%")
-
-st.metric(label="Score", value="Low", delta="-0%")
 
 # Add uploader function
 st.sidebar.subheader("File Uploader:")
 selected_file = ""
 
-uploaded_files = st.sidebar.file_uploader("Choose files",
-                                            type=["txt", "html", "css", "py", "pdf", "ipynb", "docx", "csv"],
+uploaded_file = st.sidebar.file_uploader("Choose files",
+                                            type=['.jpg', '.jpeg', '.png'],
                                             accept_multiple_files=True)
-st.sidebar.metric("Number of files uploaded", len(uploaded_files))
+if uploaded_file:
+    st.image(uploaded_file, caption='skin lesion image', width=200)
 
-if uploaded_files:
-    file_index = st.sidebar.selectbox("Select a file to display", options=[f.name for f in uploaded_files])
-    selected_file = uploaded_files[[f.name for f in uploaded_files].index(file_index)]
+# if uploaded_files:
+#     file_index = st.sidebar.selectbox("Select a file to display", options=[f.name for f in uploaded_files])
+#     selected_file = uploaded_files[[f.name for f in uploaded_files].index(file_index)]
 
 
 # Initialize chat history
@@ -49,34 +45,26 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input():
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+#Input 1 patient information: sex & age
+age = st.number_input('age', step=1)
+sex = st.multiselect('sex', ['male', 'female', 'other'])
+#input 2 patient history
+patient_history = st.text_input('patient history')
 
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# input 3 lesion characters
+lesion_char = st.text_input('lesion characters')
+
+prompt = f'the patient is  {age} years old, {sex},his history: {patient_history},the lesion characters: {lesion_char}'
+
 
 # We generate an answer only where there is a prompt
-if prompt is not None:
-    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-
-        # Formatting the prompt
-        prompt =  "<s> [INST] " + prompt +  " [/INST]"
-
-        model_prediction = Model("https://clarifai.com/mistralai/completion/models/mixtral-8x7B-Instruct-v0_1").predict_by_bytes(prompt.encode(), input_type="text", inference_params=inference_params)
-        # Take the answer
-        full_response = model_prediction.outputs[0].data.text.raw
-
-        time.sleep(0.05)
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+if st.button('generate'):
+    # Formatting the prompt
+    prompt =  "<s> [INST] " + prompt +  " [/INST]"
+            
+    model_prediction = Model("https://clarifai.com/mistralai/completion/models/mixtral-8x7B-Instruct-v0_1").predict_by_bytes(prompt.encode(), input_type="text", inference_params=inference_params)
+    # Take the answer
+    full_response = model_prediction.outputs[0].data.text.raw
+    st.write(full_response)
 
     
