0cfabd12e2fb5d7954706147dd1c34deabf656ae
Ahmed Al-rufai <aalrefaei93@gmail.com>
2024-01-12 16:52:48 +0200

    Add image classification with CNN

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import random
-import time
-import os
+import tensorflow as tf
 from clarifai.client.model import Model
 # from dotenv import load_dotenv
 # load_dotenv()
@@ -14,17 +12,47 @@ CLARIFAI_PAT = clarifai_pat
 # Model parameters or mixtral
 inference_params = dict(temperature=0.7, max_tokens=200, top_k = 50, top_p= 0.95)
 
+def get_prediction(img, Model):
+  class_names = [
+    'Chickenpox', 'Cowpox', 'HFMD', 'Healthy', 'Measles', 'Monkeypox'
+  ]
+  img = tf.keras.utils.load_img(img, target_size=(180, 180))
+  img_array = tf.keras.utils.img_to_array(img)
+  img_array = tf.expand_dims(img_array, 0)
+
+  prediction = Model.serve(img_array)
+  score = tf.nn.softmax(prediction)
+
+  return sorted([(class_names[i], 100 * score[0][i].numpy()) for i in range(len(class_names))], key=lambda x: x[1], reverse=True)
+
 st.set_page_config(page_title='Chatbot with Mixtral')
 st.title("Chatbot with Mixtral")
 
+# Caching the model for faster loading
+@st.cache_resource
+# laod the model
+def load_model(exported_model_path):
+    model = tf.saved_model.load(exported_model_path)
+    return model
+model = load_model('SkinNet')
+
 # Add uploader function
-st.sidebar.subheader("File Uploader:")
+st.sidebar.subheader("File Uploader:", divider='rainbow')
 
 uploaded_file = st.sidebar.file_uploader("Choose files",
                                             type=['.jpg', '.jpeg', '.png'],
                                             accept_multiple_files=False)
+prediction = None
 if uploaded_file:
-    st.sidebar.image(uploaded_file, caption='Image uploaded by patient', width=200)
+    st.sidebar.image(uploaded_file, caption='Image uploaded by patient', width=100)
+    classification = get_prediction(uploaded_file, model)
+    # CNN model result
+    prediction = f"it's {classification[0][0]} with {classification[0][1]} probability"
+    with st.container(border=True):
+        st.sidebar.subheader('Probabilites of Classification', divider='rainbow')
+        st.sidebar.metric(f':green[{classification[0][0]}]', value='{:.2f}%'.format(classification[0][1]))
+        st.sidebar.metric(f':green[{classification[1][0]}]', '{:.2f}%'.format(classification[1][1]))
+        st.sidebar.metric(f':green[{classification[2][0]}]', '{:.2f}%'.format(classification[2][1]))
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -33,9 +61,6 @@ if "messages" not in st.session_state:
 with st.chat_message("assistant"):
     st.write("Hello 👋, I am Galen (mixtral-8x7B-Instruct-v0_1), I'm here to help you as an AI medical assistant")
 
-
-# CNN model result
-prediction = 'melanoma' # TO CHANGE LATER
 
 #Input 1 patient information: sex & age
 age = st.number_input('age', step=1)
@@ -52,7 +77,8 @@ intro = 'Hi, I need some information about the following information'
 # lesion ="melanoma"
 action = "Please give a definition of the disease, a short description of it, the level of danger and what are the actions to take. Answer in 4 different bullet points with the 4 different titles [Definition, Description, Threat, Actions to take] ."
 
-prompt = f'<s> [INST] {intro}. I am  {age} years old, my sex is defined as{sex}. My medical history is {patient_history}. I have a {lesion_char}. It s 90% a  {prediction}. {action} "[/INST]'
+if prediction is not None:
+    prompt = f'<s> [INST] {intro}. I am  {age} years old, my sex is defined as{sex}. My medical history is {patient_history}. I have a {lesion_char}. According to CNN model prediction: {prediction}. {action} "[/INST]'
 
 
 # We generate an answer only where there is a prompt
@@ -68,7 +94,5 @@ if st.button('Generate', type="primary"):
         with st.container(border=True):
             st.write(full_response)
     except Exception as err:
-        st.exception('Somthening went worng 😢😭')
+        st.exception('You must upload an image of the skin lesion!')
         print(err)
-
-    
--- pages/french.py
+++ pages/french.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import tensorflow as tf
 from clarifai.client.model import Model
 # from dotenv import load_dotenv
 # load_dotenv()
@@ -11,8 +12,29 @@ CLARIFAI_PAT = clarifai_pat
 # Model parameters or mixtral
 inference_params = dict(temperature=0.7, max_new_tokens = 200, max_tokens=100, top_k = 50, top_p= 0.95)
 
+def get_prediction(img, Model):
+  class_names = [
+    'Chickenpox', 'Cowpox', 'HFMD', 'Healthy', 'Measles', 'Monkeypox'
+  ]
+  img = tf.keras.utils.load_img(img, target_size=(180, 180))
+  img_array = tf.keras.utils.img_to_array(img)
+  img_array = tf.expand_dims(img_array, 0)
+
+  prediction = Model.serve(img_array)
+  score = tf.nn.softmax(prediction)
+
+  return sorted([(class_names[i], 100 * score[0][i].numpy()) for i in range(len(class_names))], key=lambda x: x[1], reverse=True)
+
 st.set_page_config(page_title='Chatbot with Mixtral | French')
 st.title("Chatbot with Mixtral")
+
+# Caching the model for faster loading
+@st.cache_resource
+# laod the model
+def load_model(exported_model_path):
+    model = tf.saved_model.load(exported_model_path)
+    return model
+model = load_model('SkinNet')
 
 # Add uploader function
 st.sidebar.subheader("Importer un fichier:")
@@ -20,8 +42,18 @@ st.sidebar.subheader("Importer un fichier:")
 uploaded_file = st.sidebar.file_uploader("Sélectionner un fichier",
                                             type=['.jpg', '.jpeg', '.png'],
                                             accept_multiple_files=False)
+
+prediction = None
 if uploaded_file:
-    st.sidebar.image(uploaded_file, caption='Image importée par le patient', width=200)
+    st.sidebar.image(uploaded_file, caption='Image importée par le patient', width=100)
+    classification = get_prediction(uploaded_file, model)
+    # CNN model result
+    prediction = f"it's {classification[0][0]} with {classification[0][1]} probability"
+    with st.container(border=True):
+        st.sidebar.subheader('Probabilites of Classification', divider='rainbow')
+        st.sidebar.metric(f':green[{classification[0][0]}]', value='{:.2f}%'.format(classification[0][1]))
+        st.sidebar.metric(f':green[{classification[1][0]}]', '{:.2f}%'.format(classification[1][1]))
+        st.sidebar.metric(f':green[{classification[2][0]}]', '{:.2f}%'.format(classification[2][1]))
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -31,8 +63,6 @@ with st.chat_message("assistant"):
     st.write("Bonjour 👋, Je suis Galen (mixtral-8x7B-Instruct-v0_1), je suis ici pour vous aider comme assistant médical IA.")
 
 
-# CNN model result
-prediction = ''
 
 #Input 1 patient information: sex & age
 age = st.number_input('Age', step=1)
@@ -47,9 +77,10 @@ lesion_char = st.text_input('Caractéristique de la lésion')
 # TO DO: add CNN model prediction in the prompt
 intro = "Salut, j'ai besoin d'informations sur les éléments suivants."
 action = "S'il vous plaît, donnez une définition de la maladie, une brève description, le niveau de danger et les actions à prendre. Répondez en 4 points avec les titres suivants : [**Définition**: La maladie est définie comme..., **Description** : Il s'agit d'une maladie caractérisée par..., **Niveau de danger**: Le niveau de danger associé à cette maladie est..., **Actions à prendre** : Pour faire face à cette maladie, il est recommandé de prendre les mesures suivantes...]."
-        
-prompt = f'Le patient est agé {age} ans, {sex}, : {patient_history},the lesion characters: {lesion_char}'
-prompt = f'<s> [INST] {intro}. J ai  {age} ans, mon sexe est defini comme {sex}. Mes antecedents medicaux sont {patient_history}. J ai {lesion_char}. Le niveau de predication est de 90% {prediction}. {action} "[/INST]'
+
+if prediction is not None:
+    prompt = f'Le patient est agé {age} ans, {sex}, : {patient_history},the lesion characters: {lesion_char}'
+    prompt = f'<s> [INST] {intro}. J ai  {age} ans, mon sexe est defini comme {sex}. Mes antecedents medicaux sont {patient_history}. J ai {lesion_char}. Le niveau de predication est de 90% {prediction}. {action} "[/INST]'
 
 
 # We generate an answer only where there is a prompt
